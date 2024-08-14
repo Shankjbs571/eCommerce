fc508641bfe044b8d7870315b9d9f1a74aafc40d
AnuruddhSin <anuruddh7234@gmail.com>
2024-08-14 22:36:26 +0530

    Buttons improve

--- client/src/Pages/Grocery/Grocery.js
+++ client/src/Pages/Grocery/Grocery.js
@@ -14,14 +14,15 @@ function SideBar({ showall,filteredProducts , setShowAll, sidebarDairy, title, s
   const [toggleBar, setToggleBar] = useState(true);
 
   return (
-    <div className='max-h-screen  sticky m-4 pr-4 py-4  top-0 w-fit border-2  shadow-lg border-gray-200 '>
-      <div onClick={() => setToggleBar(!toggleBar)} className='bg-white/70 border-b-2 border-orange-700 h-12 ml-4  w-fit backdrop-blur-lg flex gap-2 items-center scale-110'>
+    <div className='max-h-screen  sticky m-2 pr-2 py-2  top-0 w-fit border-2  shadow-lg border-orange-200  '>
+      <div onClick={() => setToggleBar(!toggleBar)} className='bg-white/40 border-b-2 border-orange-900 h-10 ml-4  w-fit backdrop-blur-lg flex gap-2 items-center scale-110 px-4 py-2 rounded-full shadow-md'>
         <svg className='block md:block lg:hidden' xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
           <line x1="21" x2="3" y1="6" y2="6" />
           <line x1="15" x2="3" y1="12" y2="12" />
           <line x1="17" x2="3" y1="18" y2="18" />
         </svg>
-        <h2 onClick={() => {setActiveTab(""); setActiveSubTab(""); setActivel1Tab("")}}>{title}</h2>
+        <h2 onClick={() => {setActiveTab(""); setActiveSubTab(""); setActivel1Tab("")}}
+          className=''>{title}</h2>
       </div>
       <div className={`${toggleBar ? 'hidden' : ''}  backdrop-blur-lg lg:static lg:block ml-4  lg:w-fit mt-3 text-sm space-y-3`}>
         {sidebarDairy.map((item) => (
